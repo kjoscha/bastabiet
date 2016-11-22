236f0d92b01bdf90eef67066aa3422e25bcd88e6
andneuma <andneuma@posteo.de>
2016-11-22 23:05:31 +0100

    Prepare config files for mailer

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,17 @@ module Bastabiet
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      host: ENV['']
+      address: ENV['relay_address'],
+      port: ENV['relay_port'],
+      user_name: ENV['relay_login'],
+      password: ENV['relay_passwd'],
+      authentication: ENV['relay_auth'],
+      enable_starttls_auto: true
+    }
   end
 end
