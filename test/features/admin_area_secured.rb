e7e2df80d6c0e40c59328a8e8dd6b3312369f2d0
kjoscha <kjoscha@gmail.com>
2016-12-02 17:00:27 +0100

    change logo link when logged in

--- test/features/admin_area_secured.rb
+++ test/features/admin_area_secured.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class AdminAreaSecured < Capybara::Rails::TestCase
   scenario 'start page working' do
     visit root_path
-    assert_content 'CSA Basta'
+    assert_content 'CSA BASTA'
   end
 
   scenario 'admin area not accessible working without authentication', :js do
--- test/features/log_in.rb
+++ test/features/log_in.rb
@@ -2,16 +2,15 @@ require "test_helper"
 
 class LogIn < Capybara::Rails::TestCase
   scenario 'login possible for activated shares', :js do
-    add_valid_user(true)
-    visit root_path
-    fill_in 'Email', with: 'foo@bar.org'
-    fill_in 'Passwort', with: 'secret'
-    click_on "Ab geht's"
+    log_in
     assert_content 'Pflichtangaben'
     assert_equal 'test_skills', find_field('share_skills').value
     %w[minimum medium maximum].each do |m|
       assert_empty find_field("share_offer_#{m}").value
     end
+    click_on 'CSA BASTA'
+    refute_content ('Anmelden')
+    assert_content 'Pflichtangaben'
   end
 
   scenario 'login not possible for unregistered shares', :js do
@@ -47,5 +46,13 @@ class LogIn < Capybara::Rails::TestCase
     visit root_path
     click_on "Ab geht's"
     refute_content 'Pflichtangaben'
+  end
+
+  def log_in
+    add_valid_user(true)
+    visit root_path
+    fill_in 'Email', with: 'foo@bar.org'
+    fill_in 'Passwort', with: 'secret'
+    click_on "Ab geht's"
   end
 end
