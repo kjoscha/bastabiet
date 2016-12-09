926a897b583a35f0e00f9fd7c4be4581f89bd0c1
andneuma <andneuma@posteo.de>
2016-12-09 13:25:37 +0100

    Change link name for xls download

--- test/features/export_to_xls_test.rb
+++ test/features/export_to_xls_test.rb
@@ -3,7 +3,7 @@ require_relative '../test_helper'
 class ExportToXlsTest < Capybara::Rails::TestCase
   scenario 'XLS Download link present and working', :js do
     enter_admin_area
-    click_on 'Herunterladen als xls'
+    click_on 'Übersicht als Excel-File herunterladen'
 
     assert_equal 'application/xls; charset=utf-8', page.response_headers['Content-Type']
   end
