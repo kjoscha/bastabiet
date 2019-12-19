e6ff4c5b7ee05e0434f7d1c27b8bd4f9939cf8df
kayoshi <jo5cha@web.de>
2019-12-19 15:04:33 +0100

    add matomo

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -71,4 +71,16 @@ jQuery(function() {
     }]
   });
 
+  // Matomo
+  _paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="https://matomo.coderat.cc/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '3']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
 });
